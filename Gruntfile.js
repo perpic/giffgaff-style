c2b9d99931e5e7c232da9c66b0f3b9d09a8f5a24
Francesc Perez <f.perez.picazo@gmail.com>
2013-11-15 15:35:51 +0000

    Add grunt files

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+              ' * giffgaff styleguide v<%= pkg.version %> by @perpic\n' +
+              ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+              ' */\n\n',
+    
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/js/<%= pkg.name %>.js',
+        dest: 'build/js/<%= pkg.name %>.min.js'
+      }
+    },
+    
+    recess: {
+      options: {
+        compile: true,
+        banner: '<%= banner %>'
+      },
+      bootstrap: {
+        src: ['src/less/giffgaff.less'],
+        dest: 'build/css/<%= pkg.name %>.css'
+      },
+      min: {
+        options: {
+          compress: true
+        },
+        src: ['src/less/giffgaff.less'],
+        dest: 'build/css/<%= pkg.name %>.min.css'
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-recess');
+
+  // CSS distribution task.
+  grunt.registerTask('dist-css', ['recess']);
+
+  grunt.registerTask('build', [
+    'dist-css',
+    'uglify'
+  ]);
+
+  // Default task(s).
+  grunt.registerTask('default', [
+    'build'
+  ]);
+};
